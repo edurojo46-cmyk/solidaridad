9ae698b5c3ec8d27ea25cad4d0fb8d0d45823ba1
edurojo46-cmyk <edurojo46-cmyk@users.noreply.github.com>
2026-05-13 21:17:13 -0300

    Fix: remueve una llave adicional que causaba error de sintaxis en el archivo principal

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ﻿// SOLIDARIDAD sw.js - Version 7.0 (Network First)
-var CACHE_NAME = 'solidaridad-cache-v18';
+var CACHE_NAME = 'solidaridad-cache-v19';
 
 self.addEventListener('install', function(e) {
     self.skipWaiting(); // Force the waiting service worker to become the active service worker.
@@ -27,6 +27,7 @@ self.addEventListener('fetch', function(e) {
         })
     );
 });
+
 
 
 
